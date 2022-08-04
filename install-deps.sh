221b0416a0c4f9c6d750a051b97aee3819976a15
Isken Huang <iskenhuang@gmail.com>
2022-08-04 11:39:49 +0800

    refector install-deps and added mas cmd

--- install-deps.sh
+++ install-deps.sh
@@ -2,12 +2,31 @@
 
 # homebrew! only install on mac
 # you need the code CLI tools YOU FOOL.
-if [[ ! -z "$__MAC_VERSION" && ${#$(checkCmd brew)} > 0 ]]; then
+if [[ ! -z "$__MAC_VERSION" && $(checkCmdOne brew) == "false" ]]; then
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+    # install useful apps/cmds
+    brew install wget
+    brew install shadowsocks-rust
+    brew install v2ray-plugin
+    brew install visual-studio-code
+    brew install mas
 fi
 
+# install app from macos app store
+if [[ ! -z "$__MAC_VERSION" ]]; then
+    if [[ $(checkCmdOne mas) == "false" ]]; then
+        mas install 1091189122 #Bear
+        mas install 904280696  #Things3
+        # mas install 975937182  #Fantastical
+    else
+        echo "mas is not installed!"
+    fi
+fi
+
+
 # nvm
-if [[ ${#$(checkCmd nvm)} > 0 ]]; then
+if [[ $(checkCmdOne nvm) == "false" ]]; then
     curl https://raw.github.com/creationix/nvm/master/install.sh | sh
 
     # init nvm
