9807b454e05358f0101b264589e11e8e4b63e973
Isken Huang <iskenhuang@gmail.com>
2025-08-14 12:42:50 +0800

    Update install-deps.sh

--- install-deps.sh
+++ install-deps.sh
@@ -5,11 +5,11 @@ if [[ $(isMac) == "true" && $(checkCmdOne brew) == "false" ]]; then
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
     # install useful apps/cmds
-    brew install wget
+    # brew install wget
     # brew install shadowsocks-rust
     # brew install v2ray-plugin
     brew install visual-studio-code
-    brew install mas
+    # brew install mas
 fi
 
 # install app from macos app store
@@ -17,8 +17,8 @@ if [[ $(isMac) == "true" ]]; then
     if [[ "$(checkCmdOne mas)" == "false" ]]; then
         echo "mas is not installed!"
     else
-        mas install 1091189122 #Bear
-        mas install 904280696  #Things3
+        # mas install 1091189122 #Bear
+        # mas install 904280696  #Things3
         # mas install 975937182  #Fantastical
     fi
 fi
